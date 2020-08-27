55f7d761955160327a82b212ab9256fc83c6a226
bs03-faimrs <benjamin.sunday@faimrs.com>
2020-08-27 02:37:30 +0100

    Up to configuring dev, local and common js

---
+++ webpack.common.js
@@ -0,0 +1,17 @@
+const path = require("path");
+let HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    plugins: [ new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    }) ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    }
+}
